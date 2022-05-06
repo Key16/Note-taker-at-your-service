6dded049195940be617630ad9dad482572cc9016
Key <jh.yield@gmail.com>
2022-05-07 01:52:58 +1000

    initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+//packages required
+const express = require('express');
+const path = require('path');
+
+//paths to index route folder
+const api = require('./routes/index');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+
+// Wildcard route that returns home page all the time
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
